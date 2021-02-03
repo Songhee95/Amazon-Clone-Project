6349e41aa986e8577a219cb84eefdd0265a2127d
songhee <thdgml5537@gmail.com>
2021-02-03 17:27:02 -0500

    add firebase config and header section

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAuCSYSybWI8yRMTxs2k4jFbVGwZ-iNaM0",
+  authDomain: "clone-eec21.firebaseapp.com",
+  projectId: "clone-eec21",
+  storageBucket: "clone-eec21.appspot.com",
+  messagingSenderId: "943702347818",
+  appId: "1:943702347818:web:cc41e39ea607376c717353",
+  measurementId: "G-YL9DG5G2T2",
+};
