dcf72849f55175f2a9afc1e008af9a24c2a2f24b
songhee <thdgml5537@gmail.com>
2021-02-04 22:20:47 -0500

    add header and styling

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
@@ -12,20 +13,22 @@ function Header() {
         />
         <div className="header__search">
           <input type="text" className="header__searchInput" />
-          {/* logo */}
+          <SearchIcon className="header__searchIcon" />
         </div>
-      </div>
-      <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">Sign In</span>
+        <div className="header__nav">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
         </div>
-        <div className="header__option">
-          <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineTwo">& Orders</span>
-        </div>
-        <span className="header__optionLineOne">Your</span>
-        <span className="header__optionLineTwo">Prime</span>
       </div>
     </>
   );
