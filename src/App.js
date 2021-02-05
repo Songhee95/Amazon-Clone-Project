da8e5c7bb1ef70ab37f961c091f747d28e23ed4b
songhee <thdgml5537@gmail.com>
2021-02-05 00:46:19 -0500

    add checkout component and add styling & use currencyformat npm package"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import Header from "./component/Header/Header";
 import Home from "./component/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./component/Checkout/Checkout";
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route path="/checkout">
+              <Header />
+              <Checkout />
+            </Route>
+            <Route path="/">
+              <Header />
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
