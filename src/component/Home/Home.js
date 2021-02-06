26803e76c3b17ec44e0eb2e8153f45fae47e7eac
songhee <thdgml5537@gmail.com>
2021-02-06 17:09:31 -0500

    add reducer and make "add to basket" works

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -13,12 +13,14 @@ function Home() {
         />
         <div className="home__row">
           <Product
+            id="1"
             title="Clean Code: A Handbook of Agile Software Craftsmanship 1st Edition"
             price={49.99}
             image="https://images-na.ssl-images-amazon.com/images/I/41yafGMO+rL._SX376_BO1,204,203,200_.jpg"
             rating={5}
           />
           <Product
+            id="2"
             title="Learn JavaScript Quickly: A Complete Beginner’s Guide to Learning JavaScript, Even If You’re New to Programming (Crash Course With Hands-On Project) Paperback – November 10, 2020"
             price={18.99}
             image="https://m.media-amazon.com/images/I/61-+gHv1xAL._AC_UL480_FMwebp_QL65_.jpg"
@@ -27,18 +29,21 @@ function Home() {
         </div>
         <div className="home__row">
           <Product
+            id="3"
             title="Full-Stack React Projects: Learn MERN stack development by building modern web apps using MongoDB, Express, React, and Node.js, 2nd Edition Paperback – April 17, 2020"
             price={34.99}
             image="https://images-na.ssl-images-amazon.com/images/I/51BqNmSO6uL._SX404_BO1,204,203,200_.jpg"
             rating={4}
           />
           <Product
+            id="4"
             title="HTML and CSS: Design and Build Websites 1st Edition"
             price={29.99}
             image="https://images-na.ssl-images-amazon.com/images/I/31b4K-hFH-L._SX395_BO1,204,203,200_.jpg"
             rating={4}
           />
           <Product
+            id="5"
             title="Oracle SQL By Example 4th Edition"
             price={27.79}
             image="https://images-na.ssl-images-amazon.com/images/I/41qG9CKgUOL._SX381_BO1,204,203,200_.jpg"
@@ -47,6 +52,7 @@ function Home() {
         </div>
         <div className="home__row">
           <Product
+            id="6"
             title="The Full Stack Developer: Your Essential Guide to the Everyday Skills Expected of a Modern Full Stack Web Developer Paperback – November 20, 2018"
             price={24.49}
             image="https://images-na.ssl-images-amazon.com/images/I/41DUtSO3lIL._SX348_BO1,204,203,200_.jpg"
