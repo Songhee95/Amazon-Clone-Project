ee918d495f50f7bc9a62caf0113f4f5fd66e6e61
songhee <thdgml5537@gmail.com>
2021-02-09 00:47:51 -0500

    add user authentication with firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./component/Header/Header";
 import Home from "./component/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./component/Checkout/Checkout";
+import Login from "./component/Login/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./component/stateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    //will only run once when the app component loads...
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+      if (authUser) {
+        //the user just logged in / the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //The user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <>
       <Router>
         <div className="app">
-          <Header />
           <Switch>
-            <Route path="login">
-              <h1>login page</h1>
+            <Route path="/login">
+              <Login />
             </Route>
             <Route path="/checkout">
+              <Header />
               <Checkout />
             </Route>
             <Route path="/">
+              <Header />
               <Home />
             </Route>
           </Switch>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAuCSYSybWI8yRMTxs2k4jFbVGwZ-iNaM0",
   authDomain: "clone-eec21.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
   appId: "1:943702347818:web:cc41e39ea607376c717353",
   measurementId: "G-YL9DG5G2T2",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+//initialize database
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
